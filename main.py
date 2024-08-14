7f5b91c4579afcceaf7839d1f0823b136af49247
HELLORPG <hellorpg2017@gmail.com>
2024-08-14 20:02:16 +0800

    Add a new runtime option.

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def parse_option():
     # About outputs.
     parser.add_argument("--use-wandb", type=str)
     parser.add_argument("--outputs-dir", type=str, help="Outputs dir.")
+    parser.add_argument("--exp-owner", type=str)
     parser.add_argument("--exp-name", type=str, help="Exp name.")
     parser.add_argument("--exp-group", type=str, help="Exp group, for wandb.")
     parser.add_argument("--save-checkpoint-per-epoch", type=int)
